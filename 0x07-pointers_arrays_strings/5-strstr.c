0a33fd42dce0facca47637948751f3d659b0b943
myusername <myemail@gmail.com>
2023-04-03 20:05:32 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring.
+ * @haystack: input string
+ * @needle: input substring
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; -haystack != '\0'; haystack++)
+	{
+		char *f = haystack;
+		char *t = needle;
+
+		while (*f == *t && *t != '\0')
+		{
+			f++;
+			t++;
+		}
+
+		if (*f == '\0')
+			return (haystack);
+	}
+	return (0);
+}
