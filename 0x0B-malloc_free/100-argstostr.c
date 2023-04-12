88e8918b62651cf5b38c5c755ca44a05af5e4a35
myusername <myemail@gmail.com>
2023-04-11 18:18:54 -0700

    concat all args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * argstostr - concatenates all the arguments of the program.
+  * @ac: integer
+  * @av: char
+  * Return: 0
+  */
+char *argstostr(int ac, char **av)
+{
+	int i, j, ln = 0, arg = 0;
+	char *m;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		ln += strlen(av[i]) + 1;
+	}
+
+	m = malloc(sizeof(char) * ln);
+
+	if (m == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0' ; j++)
+		{
+			m[arg] = av[i][j];
+			arg++;
+		}
+		m[arg] = '\n';
+		arg++;
+	}
+	m[arg] = '\0';
+
+	return (m);
+}
