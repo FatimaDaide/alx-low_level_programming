0a2ea517d3158c762f9a7da599be8034f4864808
myusername <myemail@gmail.com>
2023-04-14 08:39:45 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,9 +11,31 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char 
+	void *new_p;
 	unsigned int i;
 
-	if (new_size == old_size)
-		return (ptr
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size <= old_size)
+		return (ptr);
+
+	new_p = malloc(new_size);
+
+	if (new_p == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size; i++)
+		((char *)new_p)[i] = ((char *)ptr)[i];
+
+	free(ptr);
+
+	return (new_p);
+}
 
