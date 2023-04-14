650567ddcb0bcc5d4d0eccd2ddec187419725b4b
myusername <myemail@gmail.com>
2023-04-13 19:25:45 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -32,16 +32,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
 
-	p = malloc(nmemb * sizeof(int));
+	p = malloc(nmemb * size);
 
 	if (p == 0)
 		return (NULL);
 
-	_memset(p, 0, nmemb * sizeof(int));
+	_memset(p, 0, nmemb * size);
 
 	return (p);
 }
