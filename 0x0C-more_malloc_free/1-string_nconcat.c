86d4466e95ae951960d26c511b9a29d381b0d3cb
myusername <myemail@gmail.com>
2023-04-13 18:52:58 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,15 +13,20 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i, j, len1, len2;
+	unsigned int len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-	for (len2 = 0; s2[len2] != '\0'; len2++)
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
 
 	s = malloc(len1 + n + 1);
 
@@ -30,16 +35,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 
-	for (i = 0; s1[i] != '\0'; i++)
+	while (*s1 != '\0')
 	{
-		s[i] = s1[i];
+		*s = *s1;
+		s++;
+		s1++;
 	}
-	for (j = 0; j < n; j++)
+
+	while (n > 0 && *s2 != '\0')
 	{
-		s[i] = s2[j];
-		i++;
+		*s = *s2;
+		s++;
+		s2++;
+		n--;
 	}
-	s[i] = '\0';
+	*s = '\0';
 
 	return (s);
 }
