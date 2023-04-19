65bf353c1f8fefe4a160f645697a2c7889051b87
myusername <myemail@gmail.com>
2023-04-18 18:42:24 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - information's dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  *
+  * Description: dog struct
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
