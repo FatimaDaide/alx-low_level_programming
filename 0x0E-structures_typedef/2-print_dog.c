6b54529c6ead1f84beee782db66ca8415606d772
myusername <myemail@gmail.com>
2023-04-18 19:05:30 -0700

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  * print_dog -  prints a struct dog
+  * @d: dog struct
+  */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	ptintf("Owner: %s\n" (d->owner != NULL) ? d->owner : "(nil)");
+}
